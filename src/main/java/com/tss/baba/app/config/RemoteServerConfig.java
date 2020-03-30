f748d2db5f63b07b7669f793d1526498ae556f97
yukang <wzysz555@foxmail.com>
2020-03-30 17:22:49 +0800

    update

--- src/main/java/com/tss/baba/app/config/RemoteServerConfig.java
+++ src/main/java/com/tss/baba/app/config/RemoteServerConfig.java
@@ -1,9 +1,37 @@
 package com.tss.baba.app.config;
 
+import com.tss.baba.app.model.RemoteServer;
+
+import java.util.List;
+
 /**
  * @author: akhan
  * @description:
  * @date: 1:24 2020/3/29
  */
 public class RemoteServerConfig {
+
+    private static volatile RemoteServerConfig INSTANCE;
+
+    private List<RemoteServer> serverList = null;
+
+    public static RemoteServerConfig getInstance(){
+        if(INSTANCE == null) {
+            synchronized (RemoteServerConfig.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = new RemoteServerConfig();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    private RemoteServerConfig(){
+
+    }
+
+    private void load() {
+
+    }
 }
